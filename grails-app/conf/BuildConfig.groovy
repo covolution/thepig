8e8875a8ec585adf02a52d41603a72076eac534d
Gethin James <Gethin.James@alfresco.com>
2012-03-21 20:01:42 +0000

    Added webxml 1.4.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -44,6 +44,7 @@ grails.project.dependency.resolution = {
 		runtime ":hibernate:$grailsVersion"
 		runtime ":jquery:1.7.1"
 		runtime ":resources:1.1.6"
+		runtime ":webxml:1.4.1"
 		runtime ":database-migration:1.0"
 		
 		// Uncomment these (or add new ones) to enable additional resources capabilities
